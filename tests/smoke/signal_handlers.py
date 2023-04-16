f1bebac5bff540ce3511b22518531e8047f80a20
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-16 11:41:37 +0300

    Adjusted CI tests timeout

--- tests/smoke/signal_handlers.py
+++ tests/smoke/signal_handlers.py
@@ -7,24 +7,24 @@ from celery.signals import worker_shutdown
 
 @worker_init.connect
 def worker_init_handler(sender, **kwargs):  # type: ignore
-    print(f"worker_init_handler: {sender=}")
+    print("worker_init_handler")
 
 
 @worker_process_init.connect
 def worker_process_init_handler(sender, **kwargs):  # type: ignore
-    print(f"worker_process_init_handler: {sender=}")
+    print("worker_process_init_handler")
 
 
 @worker_process_shutdown.connect
 def worker_process_shutdown_handler(sender, pid, exitcode, **kwargs):  # type: ignore
-    print(f"worker_process_shutdown_handler: {sender=}, {pid=}, {exitcode=}")
+    print("worker_process_shutdown_handler")
 
 
 @worker_ready.connect
 def worker_ready_handler(sender, **kwargs):  # type: ignore
-    print(f"worker_ready_handler: {sender=}")
+    print("worker_ready_handler")
 
 
 @worker_shutdown.connect
 def worker_shutdown_handler(sender, **kwargs):  # type: ignore
-    print(f"worker_shutdown_handler: {sender=}")
+    print("worker_shutdown_handler")
