e195e7caa4540bc9b9fb7b469ab76d3660f717d3
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-21 22:36:20 +0300

    # + (Exception,)

--- src/pytest_celery/fixtures/worker.py
+++ src/pytest_celery/fixtures/worker.py
@@ -12,7 +12,7 @@ from pytest_celery.api.components.worker import CeleryWorkerCluster
 def celery_worker(request: pytest.FixtureRequest) -> CeleryTestWorker:  # type: ignore
     worker: CeleryTestWorker = retry_call(
         lambda: request.getfixturevalue(request.param),
-        exceptions=defaults.COMPONENT_RETRYABLE_ERRORS + (Exception,),
+        exceptions=defaults.COMPONENT_RETRYABLE_ERRORS,
         max_delay=defaults.COMPONENT_RETRYABLE_DELAY,
     )
     # worker: CeleryTestWorker = request.getfixturevalue(request.param)
