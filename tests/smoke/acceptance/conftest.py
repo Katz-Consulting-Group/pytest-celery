b750075fb10d679839dc8b07f03a7906fc553fe7
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-22 00:29:14 +0300

    no retry_call smoke acceptance

--- tests/smoke/acceptance/conftest.py
+++ tests/smoke/acceptance/conftest.py
@@ -1,12 +1,13 @@
 from typing import Tuple
 
 import pytest
-from retry.api import retry_call
 
-from pytest_celery import defaults
+# from pytest_celery import defaults
 from pytest_celery.api.components.worker.cluster import CeleryWorkerCluster
 from pytest_celery.api.components.worker.node import CeleryTestWorker
 from tests.common.celery4.fixtures import *  # noqa
+
+# from retry.api import retry_call
 
 
 @pytest.fixture(
@@ -18,14 +19,14 @@ from tests.common.celery4.fixtures import *  # noqa
     ]
 )
 def celery_worker_cluster(request: pytest.FixtureRequest) -> CeleryWorkerCluster:
-    nodes: Tuple[CeleryTestWorker] = tuple(
-        retry_call(
-            lambda: [request.getfixturevalue(worker) for worker in request.param],
-            exceptions=defaults.COMPONENT_RETRYABLE_ERRORS,  # + (Exception,),
-            max_delay=defaults.COMPONENT_RETRYABLE_DELAY,
-        )
-    )
-    # nodes: Tuple[CeleryTestWorker] = [request.getfixturevalue(worker) for worker in request.param]
+    # nodes: Tuple[CeleryTestWorker] = tuple(
+    #     retry_call(
+    #         lambda: [request.getfixturevalue(worker) for worker in request.param],
+    #         exceptions=defaults.COMPONENT_RETRYABLE_ERRORS,  # + (Exception,),
+    #         max_delay=defaults.COMPONENT_RETRYABLE_DELAY,
+    #     )
+    # )
+    nodes: Tuple[CeleryTestWorker] = [request.getfixturevalue(worker) for worker in request.param]
     cluster = CeleryWorkerCluster(*nodes)
     cluster.ready()
     yield cluster
