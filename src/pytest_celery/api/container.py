9b5d73db01a01e1ab7589bc3673eb0fd59c54c48
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-22 20:24:29 +0300

    # timeout=defaults.READY_TIMEOUT

--- src/pytest_celery/api/container.py
+++ src/pytest_celery/api/container.py
@@ -34,7 +34,7 @@ class CeleryTestContainer(wrappers.Container):
         wait_for_callable(
             f">>> Warming up: '{self.__class__.__name__}::{self.name}'",
             super().ready,
-            timeout=defaults.READY_TIMEOUT,
+            # timeout=defaults.READY_TIMEOUT,
         )
         ready = super().ready()
 
