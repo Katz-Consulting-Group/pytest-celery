2da71c9e489feba7997d0a62af418aec9cf5b56d
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-23 19:12:42 +0300

    RABBITMQ_CONTAINER_TIMEOUT = 42

--- src/pytest_celery/defaults.py
+++ src/pytest_celery/defaults.py
@@ -211,7 +211,7 @@ DEFAULT_REDIS_BROKER_PORTS = REDIS_PORTS
 RABBITMQ_IMAGE = "rabbitmq:latest"
 RABBITMQ_PORTS = {"5672/tcp": None}
 RABBITMQ_ENV: dict = {}
-RABBITMQ_CONTAINER_TIMEOUT = READY_TIMEOUT
+RABBITMQ_CONTAINER_TIMEOUT = 42
 
 # Docker containers settings
 #################################################
