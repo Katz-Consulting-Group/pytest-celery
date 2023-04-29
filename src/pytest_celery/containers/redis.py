cf67c7c9e631fa2983f3ac5615494639dff1f827
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-30 01:12:16 +0300

    Removed ready_prompt from redis and rabbitmq

--- src/pytest_celery/containers/redis.py
+++ src/pytest_celery/containers/redis.py
@@ -62,7 +62,3 @@ class RedisContainer(CeleryTestContainer):
     @classmethod
     def ports(cls) -> dict:
         return defaults.DEFAULT_REDIS_BACKEND_PORTS
-
-    @property
-    def ready_prompt(self) -> str:
-        return "Ready to accept connections"
