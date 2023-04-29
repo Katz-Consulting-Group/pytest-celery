12996986e38821f99f170db7c1002d6e744e5110
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-29 14:24:35 +0300

    gc.collect(0)

--- src/pytest_celery/components/backend/redis/api.py
+++ src/pytest_celery/components/backend/redis/api.py
@@ -5,5 +5,5 @@ from pytest_celery.fixtures.backend import CeleryTestBackend
 
 class RedisTestBackend(CeleryTestBackend):
     def teardown(self) -> None:
-        gc.collect()  # TODO: Explain why this is needed
+        gc.collect(0)  # TODO: Explain why this is needed
         super().teardown()
