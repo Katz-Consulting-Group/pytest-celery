394aa0fd35b5305543fe5f952b76234ea2e364b6
Tomer Nosrati <d6.tomer@gmail.com>
2023-04-29 13:55:17 +0300

    moved gc.collect() from general backend to redis backend

--- src/pytest_celery/components/backend/redis/api.py
+++ src/pytest_celery/components/backend/redis/api.py
@@ -1,5 +1,9 @@
+import gc
+
 from pytest_celery.fixtures.backend import CeleryTestBackend
 
 
 class RedisTestBackend(CeleryTestBackend):
-    pass
+    def teardown(self) -> None:
+        gc.collect()  # TODO: Explain why this is needed
+        super().teardown()
